554282a781722cb7647b81d52270690c2e90b362
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-03 11:34:25 +0300

    added arrays part1

---
+++ js_sessions/arraysPart1/index.js
@@ -0,0 +1,113 @@
+// начальный массив
+const numbersList = [1, 25, 3884, -9, 5];
+
+/* ==================================== */
+/* ===> мутирующие методы массивов <=== */
+/* ===> изменяют исходный массив <===== */
+/* ==================================== */
+
+
+/* pop - удалит из массива последний элемент и вернет его */
+
+// c помощью метода pop достаньте последний элемент массива и поместите его в переменную lastNumber
+// выведите эту переменную в консоль
+
+// ... code here
+
+console.log('Before ' + numbersList );
+
+numbersList.pop();
+console.log('After ' + numbersList );
+
+
+/* push - добавит элемент в конец массива */
+
+// c помощью метода push добавьте число 6 в конец массива
+// выведите numbersList в консоль
+
+// ... code here
+console.log('Before ' + numbersList );
+
+const res = numbersList.push(6);
+console.log(res);
+
+console.log('After ' + numbersList );
+
+/* shift - удалит из массива первый элемент и вернет его */
+
+// c помощью метода shift достаньте первый элемент массива и поместите его в переменную firstNumber
+// выведите эту переменную в консоль
+
+// ... code here
+console.log('Before ' + numbersList );
+
+numbersList.shift();
+
+console.log('After ' + numbersList );
+
+/* unshift - добавит элемент в начало массива */
+
+// c помощью метода unshift добавьте число 0 в начало массива
+// выведите numbersList в консоль
+
+// ... code here
+
+console.log('Before ' + numbersList );
+
+const resultUnshift = numbersList.unshift(111);
+console.log(resultUnshift);
+console.log('After ' + numbersList );
+
+
+// sort
+
+console.log('Before ' + numbersList );
+
+const resultSort = numbersList.sort((a, b) => a - b);
+console.log('Sort ' + resultSort);
+
+console.log('After ' + numbersList );
+
+/* ======================================= */
+/* ===> НЕ мутирующие методы массивов <=== */
+/* ===> НЕ изменяют исходный массив <===== */
+/* ======================================= */
+
+
+/* метод map */
+/* const newArr = arr.map(callback) - создает и возвращает новый массив. Длина нового и исходного массивов равны */
+/* ф-ция callback запустится по очереди для каждого элемента начального массива arr */
+/* элементы массива newArr - это результат вызова callback для каждого элемента arr */
+/* newArr[i] = callback(arr[i]) */
+
+// c помощью метода map создайте новый массив squaredNumbers, в котором будут квадраты чисел из numbersList
+// выведите squaredNumbers в консоль
+
+// ... code here
+
+const squareNumber = numbersList.map(num => num * num);
+console.log('MapResult ' + squareNumber);
+console.log(numbersList);
+/* метод filter */
+/* const newArr = arr.filter(callback) - создает и возвращает новый отфильтрованный массив. */
+/* ф-ция callback запустится по очереди для каждого элемента начального массива arr */
+/* в newArr попадут только те элементы arr, для которых callback(arr[i]) вернет true */
+/* callback(arr[i]) === true -> элемент будет добавлен в финальный массив  */
+/* callback(arr[i]) === false -> элемент НЕ будет добавлен в финальный массив  */
+
+// c помощью метода filter создайте новый массив evenNumbers, в котором только четные числа из numbersList
+// выведите evenNumbers в консоль
+
+// ... code here
+
+const filterRes = numbersList.filter(num => num > 50);
+console.log('FilteredResult ' + filterRes);
+console.log(numbersList);
+/* метод find */
+/* const arrElement = arr.filter(callback) - вернет первый элемент массива arr, который удовлетворяет условию в callback. */
+/* ф-ция callback запустится по очереди для каждого элемента начального массива arr */
+/* если callback(arr[i]) вернет true, то filter прекратит поиск и вернет этот элемент */
+
+// c помощью метода find найдите первое нечетное число в numbersList и выведите его в консоль
+
+// ... code here
