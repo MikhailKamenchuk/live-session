c513ab3cbc60d0608557930e76c85692d0f2aed6
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-04 11:32:33 +0300

    added arrays part2

---
+++ js_sessions/arraysPart2/index.js
@@ -0,0 +1,22 @@
+// algo 
+// create empty array
+// iterate array & call callback
+
+// input: [10, 20, 30, 1, 8]
+// output: [30]
+
+function filterArray (arr, callback) {
+    const result = [];
+    
+    for(let i = 0; i < arr.length; i++){
+        if(callback(arr[i], i, arr)) result.push(arr[i])
+    }
+    
+    return result
+}
+
+let array = [10, 30, 1, 8, 122];
+const callback = element => element > 20;
+
+console.log(filterArray (array, callback))
+
