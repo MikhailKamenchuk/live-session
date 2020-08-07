15bb6c241b6d0266eac29b78b6a9e64d6f027488
Mikhail Kamenchuk <35965806+MikhailKamenchuk@users.noreply.github.com>
2020-08-07 10:20:14 +0300

    Update index.js

--- js_sessions/objects/task2/index.js
+++ js_sessions/objects/task2/index.js
@@ -3,12 +3,7 @@
 
 // algo:
 // 1. iterate keysList with reduce
-// 1. iterate keysList with reduce
 
-const buildObject = (keysList, valuesList) => {
-    return keysList.reduce((acc,next,index) => {
-        const newElem = {[next]: valuesList[index]}
-        console.log(newElem)
-        return  Object.assign(acc, newElem);
-    }, {})
-};
+
+const buildObject = (keysList, valuesList) => keysList.reduce((acc,next,index) => Object.assign(acc, {[next]: valuesList[index]}), {});
+
