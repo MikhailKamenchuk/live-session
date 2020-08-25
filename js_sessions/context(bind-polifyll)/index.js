e786f90c39439aea17ecd4b9d02d8216d40a94ad
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-08-25 11:31:13 +0300

    added context(bind-polyfill)

--- js_sessions/context(bind-polifyll)/index.js
+++ js_sessions/context(bind-polifyll)/index.js
@@ -3,6 +3,7 @@ function printMessage(age, city) {
   console.log(this);
   console.log(`Hello ${this.name}, email ${this.email}, age: ${age}, city: ${city}`);
 }
+
 let user = {
   name: "Alex",
   email: "gmail.com",
@@ -12,16 +13,49 @@ let user = {
 //input: obj, aarg1,..., argN
 //output: function
 
-const pmBinded = printMessage.bind(user);
-pmBinded(100, 'Lviv')
+// const pmBinded = printMessage.bind(user);
+// pmBinded(100, 'Lviv')
 
 
 // bind algs
 //input: func, obj, aarg1,..., argN
 //output: function
-const myBind = (func, context, ...args) => {
+// const myBind = (func, context, ...args) => {
+//   console.log('Step1')
+//   return function() {
+//     console.log('Step1')
+//     return func.call(context, ...args)
+//   }
+// }
+// const myBind = myBind(printMessage, user, 200, 'Kiyv');
 
-  // TODO
+// const myBind1 = (func, context, ...args) => {
+//   console.log('Step1')
+//   return function(...targetArgs) {
+//     console.log('Step1')
+//     return func.call(context, ...args, ...targetArgs)
+//   }
+// }
+
+// const printMyBind1 = myBind1(printMessage, user);
+// printMyBind1(200, 'Kiyv');
+// const printMyBind2 = myBind1(printMessage, user, 200);
+// printMyBind2( 'Kiyv')
+
+const myBindSuperBind = (func, context, ...args) => {
+  console.log("step1")
+  return function () {
+    console.log("step2");
+
+    const copyContext = {...context}
+    copyContext.tempFunction = func;
+    copyContext.tempFunction(...args);
+  }
+
 }
 
-// myBind(printMessage, user, 200, 'Kiyv'))();
+const printMyBind4 = myBindSuperBind(printMessage, user, 200, 'Dnipro');
+// console.log(printMyBind4)
+printMyBind4();
+
+console.log(user)
